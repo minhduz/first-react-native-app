a62a0f601bba7eb576d5dd365c6b9296be8d9c35
minhduz <minhducoverwatch@gmail.com>
2024-03-19 22:43:10 +0700

    first commit

---
+++ api/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+const app = express();
+const port = 8000;
+const cors = require("cors");
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+
+const jwt = require("jsonwebtoken");
+
+mongoose
+.connect(
+    "mongodb+srv://minhducoverwatch:minhduc@cluster0.dtoneke.mongodb.net/",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology:true
+    }
+).then(() => {
+    console.log("Connected to Mongo Db")
+}).catch((err) => {
+    console.log("Error connecting to MongoDb")
+})
+
+app.listen(port, () =>{
+    console.log("Server running on port 8000")
+})
+
+
