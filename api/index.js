5c864e4a8e931044ef8a6eb66b30cb2058c930db
minhduz <minhducoverwatch@gmail.com>
2024-03-20 09:12:11 +0700

    update register

--- api/index.js
+++ api/index.js
@@ -16,20 +16,43 @@ app.use(passport.initialize());
 const jwt = require("jsonwebtoken");
 
 mongoose
-.connect(
+  .connect(
     "mongodb+srv://minhducoverwatch:minhduc@cluster0.dtoneke.mongodb.net/",
     {
-        useNewUrlParser: true,
-        useUnifiedTopology:true
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     }
-).then(() => {
-    console.log("Connected to Mongo Db")
-}).catch((err) => {
-    console.log("Error connecting to MongoDb")
-})
+  )
+  .then(() => {
+    console.log("Connected to Mongo Db");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDb");
+  });
 
-app.listen(port, () =>{
-    console.log("Server running on port 8000")
-})
+app.listen(port, () => {
+  console.log("Server running on port 8000");
+});
 
+const User = require("./models/user");
+const Message = require("./models/message");
 
+//endpoint for registration of the user
+
+app.post("/register", (req, res) => {
+  const { name, email, password, image } = req.body;
+
+  // create a new User object
+  const newUser = new User({ name, email, password, image });
+
+  // save the user to the database
+  newUser
+    .save()
+    .then(() => {
+      res.status(200).json({ message: "User registered successfully" });
+    })
+    .catch((err) => {
+      console.log("Error registering user", err);
+      res.status(500).json({ message: "Error registering the user!" });
+    });
+});
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -9,6 +9,7 @@ import {
 } from "react-native";
 import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
 
 const RegisterScreen = () => {
   const [email, setEmail] = useState("");
@@ -16,6 +17,38 @@ const RegisterScreen = () => {
   const [password, setPassword] = useState("");
   const [image, setImage] = useState("");
   const navigation = useNavigation();
+
+  const handleRegister = () => {
+    const user = {
+      name: name,
+      email: email,
+      password: password,
+      image: image,
+    };
+
+    // send a POST  request to the backend API to register the user
+    axios
+      .post("http://localhost:8000/register", user)
+      .then((response) => {
+        console.log(response);
+        Alert.alert(
+          "Registration successful",
+          "You have been registered Successfully"
+        );
+        setName("");
+        setEmail("");
+        setPassword("");
+        setImage("");
+      })
+      .catch((error) => {
+        Alert.alert(
+          "Registration Error",
+          "An error occurred while registering"
+        );
+        console.log("registration failed", error);
+      });
+  };
+
   return (
     <View
       style={{
@@ -125,6 +158,7 @@ const RegisterScreen = () => {
           </View>
 
           <Pressable
+            onPress={handleRegister}
             style={{
               width: 200,
               backgroundColor: "#4A55A2",
